d32eac6068680916eb5eb00d69a441c50d913a18
EuReinoso <lucas.reinoso.br@gmail.com>
2021-04-17 10:02:09 -0300

    change stick to lines bases

--- stick.py
+++ stick.py
@@ -1,19 +1,27 @@
-import pygame
+import pygame, math
 
 pygame.init()
 
 class Stick:
-    def __init__(self,size,pos):
-        self.pos = pos
-        self.size = size
-        self.surface = pygame.Surface(size)
+    def __init__(self, p_1, p_2,sons_quant= 1):
+        self.p1 = p_1
+        self.p2 = p_2
+        self.sons_quant = sons_quant
         self.sons = []
-        self.sons_quant = 1
 
-        self.init_sons()
+        if self.get_length() > 1:
+            self.init_sons()
+
+    def draw(self,window, color):
+        pygame.draw.line(window, color, self.p1, self.p2)
 
     def init_sons(self):
         for i in range(self.sons_quant):
-            size = (self.size[0], self.size[1]/2)
-            pos = self.pos
-            sons.append(Stick(size,pos))
+            p1 = (self.p2[0], self.p2[1])
+            p2 = (self.p2[0], self.p2[1] + self.get_length()/2)
+            self.sons.append(Stick(p1, p2))
+
+    def get_length(self):
+        x = self.p1[0] - self.p2[0]
+        y = self.p1[1] - self.p2[1]
+        return math.pow(math.pow(x,2) + math.pow(y,2),1/2)

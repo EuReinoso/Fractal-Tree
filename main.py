ee467bd6b909fb9c050d9742f74994c9e34437c6
EuReinoso <lucas.reinoso.br@gmail.com>
2021-04-19 19:59:05 -0300

    move

--- main.py
+++ main.py
@@ -14,6 +14,30 @@ angle_increment = 45
 length_scale = 0.65
 level = 10
 
+is_angle_increment_up = False
+is_angle_increment_down = False
+is_length_scale_up = False
+is_length_scale_down = False
+is_level_up = False
+is_level_down = False
+
+def move():
+    global angle_increment, length_scale, level
+    if is_angle_increment_up:
+        angle_increment += 0.5
+    if is_angle_increment_down:
+        angle_increment -= 0.5
+
+    if is_length_scale_up:
+        length_scale += 0.001
+    if is_length_scale_down:
+        length_scale -= 0.001
+
+    if is_level_up:
+        level += 0.1
+    if is_level_down:
+        level -= 0.1
+
 while True:
     window.fill((0, 0, 0))
 
@@ -23,16 +47,36 @@ while True:
             sys.exit()
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_UP:
-                length_scale += 0.01
+                is_length_scale_up = True
             if event.key == pygame.K_DOWN:
-                length_scale -= 0.01
+                is_length_scale_down = True
             if event.key == pygame.K_RIGHT:
-                angle_increment += 2
+                is_angle_increment_up = True
             if event.key == pygame.K_LEFT:
-                angle_increment -= 2
-        
+                is_angle_increment_down = True
+            if event.key == pygame.K_w:
+                is_level_up = True
+            if event.key == pygame.K_s:
+                is_level_down = True
 
-    tree = Tree(angle_increment= angle_increment, length_scale= length_scale)
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_UP:
+                is_length_scale_up = False
+            if event.key == pygame.K_DOWN:
+                is_length_scale_down = False
+            if event.key == pygame.K_RIGHT:
+                is_angle_increment_up = False
+            if event.key == pygame.K_LEFT:
+                is_angle_increment_down = False
+            if event.key == pygame.K_w:
+                is_level_up = False
+            if event.key == pygame.K_s:
+                is_level_down = False
+
+    tree = Tree(angle_increment= angle_increment, length_scale= length_scale, width= 2)
     tree.tree_init(window,pos_init, 90, level= level)
+
+    move()
+
     pygame.display.update()
     pygame.display.flip()
